c30bea7521b3566bfaa94ac387d663a7382984e3
Parinith <parinith99@gmail.com>
2024-06-27 21:07:32 +0530

    Added my project

--- app.py
+++ app.py
@@ -36,14 +36,14 @@ Questions:{input}
 def vector_embedding():
 
     if "vectors" not in st.session_state:
-
-        st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-        st.session_state.loader=PyPDFDirectoryLoader("./docs") ## Data Ingestion
-        st.session_state.docs=st.session_state.loader.load() ## Document Loading
-        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) ## Chunk Creation
-        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20]) #splitting
-        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings) #vector OpenAI embeddings
-
+        with st.spinner('Loading documents Please wiat'):
+            st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+            st.session_state.loader=PyPDFDirectoryLoader("./docs") ## Data Ingestion
+            st.session_state.docs=st.session_state.loader.load() ## Document Loading
+            st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) ## Chunk Creation
+            st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20]) #splitting
+            st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings) #vector OpenAI embeddings
+        st.success('Documents loaded successfully')
 
 
 
